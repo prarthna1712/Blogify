3b1e836c0708cc94c2fe6960773103453da713fd
Prarthana Bhalerao <bhaleraoprarthana03@gmail.com>
2024-10-31 19:51:58 +0530

    navbar js

--- components/Navbar.js
+++ components/Navbar.js
@@ -16,7 +16,7 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="p-4 bg-background/50 sticky top-0 border-b backdrop-blur">
+    <nav className="p-4 bg-background/50 sticky top-0 border-b backdrop-blur z-10">
       <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
         <div className="relative flex items-center justify-between h-16">
           <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
