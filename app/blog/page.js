7a4331ac97617a0165dd9e7187c60717865a70df
Prarthana Bhalerao <bhaleraoprarthana03@gmail.com>
2024-11-06 20:29:12 +0530

    blog.js page

--- app/blog/page.js
+++ app/blog/page.js
@@ -24,39 +24,39 @@ const Blog = () => {
   ];
 
   return (
-    <div className="container mx-auto px-4 py-10 dark:bg-gray-200">
-      <h1 className="text-3xl font-bold text-center mb-8 dark:text-gray-700">
-        Our Blog
-      </h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-        {blogs.map((blog, index) => (
-          <div
-            key={index}
-            className="bg-white rounded-lg shadow-md overflow-hidden transform transition duration-500 hover:scale-105"
-          >
-            <img
-              src={blog.image}
-              alt={blog.title}
-              className="w-full h-48 object-cover"
-            />
-            <div className="p-4">
-              <h2 className="text-xl font-semibold mb-2">{blog.title}</h2>
-              <p className="text-gray-600 mb-4">{blog.description}</p>
-              <div className="text-gray-500 text-sm mb-2">
-                <p>By {blog.author}</p>
-                <p>{new Date(blog.date).toLocaleDateString()}</p>
+    <>
+      <div className="container mx-auto px-4 py-10 dark:bg-gray-200">
+        <h1 className="text-3xl font-bold text-center mb-8 dark:text-gray-700">
+          Our Blog
+        </h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          {blogs.map((blog, index) => (
+            <div
+              key={index}
+              className="bg-white rounded-lg shadow-md overflow-hidden transform transition duration-500 hover:scale-105"
+            >
+              <img
+                src={blog.image}
+                alt={blog.title}
+                className="w-full h-48 object-cover"
+              />
+              <div className="p-4">
+                <h2 className="text-xl font-semibold mb-2">{blog.title}</h2>
+                <p className="text-gray-600 mb-4">{blog.description}</p>
+                <div className="text-gray-500 text-sm mb-2">
+                  <p>By {blog.author}</p>
+                  <p>{new Date(blog.date).toLocaleDateString()}</p>
+                </div>
+                <button className="w-full px-4 py-2 mt-6 text-white bg-primary rounded-lg hover:bg-primary/70 focus:outline-none dark:bg-primary dark:hover:bg-primary/80 dark:text-black">
+                  Read More →
+                </button>
               </div>
-              <a
-                href={`/blog/${blog.slug}`}
-                className="text-indigo-600 hover:text-indigo-800 font-semibold"
-              >
-                Read more →
-              </a>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
+      <div></div>
+    </>
   );
 };
 
